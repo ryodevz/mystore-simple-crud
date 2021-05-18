34c12e1c2c717d6033a86958925e495c3e15d6bc
RyoDev <ytryo789@gmail.com>
2021-05-18 20:12:40 +0800

    Fix

--- bootstrap/core.php
+++ bootstrap/core.php
@@ -48,4 +48,4 @@ require_once __DIR__ . '/../Illuminate/Support/Str.php';
 |--------------------------------------------------------------------------
 |
 */
-if (getV() < base64_decode('Ny40LjE1')) return die('VGhlIG1pbmltdW0gcGhwIHZlcnNpb24gcmVxdWlyZWQgdG8gcnVuIHRoaXMgYXBwbGljYXRpb24gaXMgNy40LjE1');
+if (getV() < base64_decode('Ny40LjE1')) return die(base64_decode('VGhlIG1pbmltdW0gcGhwIHZlcnNpb24gcmVxdWlyZWQgdG8gcnVuIHRoaXMgYXBwbGljYXRpb24gaXMgNy40LjA='));
